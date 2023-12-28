3705fa74588d763cedf6b553d86b8bda451a3f19
Deepak Sekarbabu <deepakinmail@gmail.com>
2023-12-28 16:41:54 +0100

    Update UserRepository.java

--- src/main/java/com/deepak/mongoreactive/repository/UserRepository.java
+++ src/main/java/com/deepak/mongoreactive/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.deepak.mongoreactive.repository;
 
 import com.deepak.mongoreactive.models.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,6 +9,9 @@ import reactor.core.publisher.Mono;
 import java.time.LocalDateTime;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+
+
+    Flux<User> findAllBy(Pageable pageable);
 
     Mono<User> findByPhoneNumber(String phoneNumber);
 
